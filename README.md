9cae158c6137e057bb847640e3272ed1b8294452
Erick Oliveira <erickoliveirasystem@outlook.com>
2020-10-15 20:32:24 -0300

    Seconde day

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Happy-Server
+
+## Initialization
+* `npm init`
+* `npm install express`
+* `npm install typescript -d`
+* `npx tsc --init`
+* Changed the target at the tsconfig.json file from es5 to es2017 to reduce unnecessary convertions.
+* `npm install ts-node-dev -d` Is used to allow the use of typescript and node.
+* Added the dev script. Flags used:
+    * Transpile-only: disable the constant auto check made by ts-node-dev.
+    * Ignore-watch node_modules:  disable the constant auto check made in the node modules.
+
+* ` npm install typeorm sqlite3`
+
+## Tsconfig:
+* Set the "strictPropertyInitialization" to false for ts to not request a initial value as a initialization (this is important when making models).
+* Set "experimentalDecorators" and "emitDecoratorMetadata" to true to alow using decorators (also important when making models);
+
+# Database, ORM and typeorm
+* Created an ormconfig.json
+
+## ORM
+In the ormconfig.json, it is set:
+    * Where the database is;
+    * The type of the database;
+    * Where the migrations will be stored;
+    * Where the cli should store the migrations;
+    * Where the entites (models) are;
+
+## Typeorm
+Typeorm runs in javascript by default. So, it's necessary to overwrite the default script on the package.json.
+
+### Migrations
+* Create a migration:
+`npx typeorm migration:create -n (name_of_the_migration)`
+* Run migrations:
+`npm run typeorm migration:run`
+* Revert migrations:
+`npm run typeorm migration:revert`
+
+## Multer and @types/multer
+It's a middleware used to handle image and files sent to the body. (I have a long and painfull history with this middleware). 
+
+# Exception error handler and express-async-errors
+This module is used to handle async errors. Async errors usually take too long or sometimes doesn't return anything until a timeout happens.  
+
+# Validation: yup and @types/yup
+This module helps to simplify the validation of data.
+
+# Cors
+Allow sites with diferents urls to access the server. It's recomended to install as dependency in production because node just allow the trasnfer of data when the the server and frontend are in the same url.
---
+++ src/controllers/OrphanagesController.ts
@@ -0,0 +1,105 @@
+import { getRepository } from 'typeorm';
+import { Request, Response } from "express";
+import orphanageView from '../views/orphanages_view';
+//Yupe doesn't have export default so that's the way to import
+import * as Yup from 'yup';
+
+import Orphanage from '../models/Orphanage';
+
+export default {
+    async index(req: Request, res: Response) {
+        const orphanagesRepository = getRepository(Orphanage);
+
+        // try {
+            const orphanages = await orphanagesRepository.find({
+                relations: ['images']
+            });
+    
+            return res.status(200).json(orphanageView.renderMany(orphanages));
+            
+        // } catch (error) {
+        //     return res.status(500).send(error); 
+        // }
+    },
+
+    async show(req: Request, res: Response) {
+        const { id } = req.params;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        try {
+            const orphanages = await orphanagesRepository.findOneOrFail(id, {
+                relations: ['images']
+            });
+    
+            return res.status(200).json(orphanageView.render(orphanages));
+            
+        } catch (error) {
+            return res.status(404).send(error); 
+        }
+    },
+
+    async create(req: Request, res: Response) {
+        const {
+            name, 
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+        } =  req.body;
+        
+        const orphanagesRepository = getRepository(Orphanage);
+
+        //Way to fix the type of the multer format
+        const requestImages = req.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
+        const data = {
+            name, 
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+            images
+        };
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({                
+                    path: Yup.string().required()
+                })
+            )
+        })
+
+        //We set abort early as false to analyse every error to return to the frontend
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+        //Makes a database object
+        const orphanage = orphanagesRepository.create(data);
+
+        try {
+            await orphanagesRepository.save(orphanage);
+            
+            return res.status(201).send();
+            
+        } catch (error) {
+    
+            return res.status(500).send(error);
+        }
+    }
+}
